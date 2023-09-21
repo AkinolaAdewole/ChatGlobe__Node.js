2db611dcd9860c1757bb0dc492012cf3d5032f26
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-21 18:08:31 +0100

    index.js

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 import express from 'express';
-import http from 'http'; // Import the 'http' module, not 'https'
+import http from 'http';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename); // Obtain the directory name using path module
 
 const app = express();
 const server = http.createServer(app);
 
-// app.get('/', (req, res) => {
-//   res.send({ message: "Hello world" }); // JSON response
-// });
-
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the 'public' directory
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html'); // Serve an HTML file
+  res.sendFile(path.join(__dirname, 'index.html')); // Serve an HTML file
 });
 
-const PORT = 3200;
+const PORT = 3800;
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
