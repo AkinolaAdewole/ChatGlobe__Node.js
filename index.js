e05777b02b3ad4172a55588d769859aea22f4594
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-21 11:40:36 +0100

    Node chat App

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import express from 'express';
-const app = express();
+import http from 'http'; // Import the 'http' module, not 'https'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-import http from 'https'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
 const server = http.createServer(app);
 
-// app.get('/', (req,res)=>{
-//     res.send({message:"Hello world"});
+// app.get('/', (req, res) => {
+//   res.send({ message: "Hello world" }); // JSON response
 // });
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/index.html')
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html'); // Serve an HTML file
 });
 
-const PORT= process.env.PORT 
-server.listen(PORT, ()=>{
-    console.log(`Server is listening on port ${PORT}`);
-})
+const PORT = 3200;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
