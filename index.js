bc0d115ef59c59666cf6bfaae4f9c406074d7d93
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-21 23:10:59 +0100

    index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import http from 'http';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { Server } from 'socket.io';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename); // Obtain the directory name using path module
@@ -13,6 +15,19 @@ app.use(express.static(path.join(__dirname, 'public'))); // Serve static files f
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html')); // Serve an HTML file
+});
+
+const io = new Server(server);
+
+// Define Socket.IO event handlers here
+io.on('connection', (socket) => {
+  console.log('A user connected'); // This will log when a user connects
+
+  // Define other event handlers or logic here
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected'); // This will log when a user disconnects
+  });
 });
 
 const PORT = 3800;
